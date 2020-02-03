36140132a56e36f57b4aabdc04b57034fe2b735b
Scott Herold <sherold@mail.usf.edu>
2020-02-03 10:53:12 -0500

    The useState hook

--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+const App = (props) => {
+    /*
+    / with useState(), you can provide any argument (string, number, or object)
+    / This is unlike class components, and state, which requires an object
+    / This returns an array of two items: the state, and the function to manipulate the state
+    / the most common way of accessing array data is by destructing the array (see below syntax)
+    */
+    const [count, setCount] = useState(props.count);
+
+    return (
+        <div>
+            <p>The current count is {count}</p>
+            <button onClick={() => setCount(count - 1)}>-1</button>
+            <button onClick={() => setCount(count = 0)}>Reset</button>
+            <button onClick={() => setCount(count + 1)}>+1</button>
+        </div>
+    )
+}
+
+// This is how to setup default props
+App.defaultProps = {
+    count: 0
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
